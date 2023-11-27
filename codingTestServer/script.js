23b59bb705b6f0c7a343194a17ad70c1184803a9
강승민 <rkdtmdals999@naver.com>
2023-11-27 20:13:34 +0900

    [feat] 코드 제출 후에도 보이도록 설정

--- codingTestServer/script.js
+++ codingTestServer/script.js
@@ -1,40 +1,3 @@
-// async function sendData() {
-//     try {
-//         // const inputValue = document.getElementById('inputCode').value;
-//         const inputValue = await editor.getValue(); // CodeMirror 에디터에서 코드를 가져오도록 수정
-
-//         // 입력한 텍스트를 로컬 스토리지에 저장하여 나중에 유지합니다.
-//         localStorage.setItem('savedCode', inputValue);
-
-//         console.log(inputValue);
-        
-//         // 서버로 입력한 정수 보내기
-//         await fetch('http://localhost:8080', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'text/plain;charset=UTF-8',
-//             },
-//             body: inputValue
-//         })
-//         .then(async response => {
-//             if (!response.ok) {
-//                 throw new Error(`HTTP error! Status: ${response.status}`);
-//             }
-//             return await response.text();
-//         })
-//         .then(data => {
-//             document.getElementById('result').textContent = `${data}`;
-//             console.log('result:', data);
-//             localStorage.setItem('lastResult', data);
-//         })
-//         .catch(error => {
-//             console.error('오류:', error);
-//         });
-//     } catch (error) {
-//         console.error("오류:", error);
-//     }
-// }
-
 async function sendData() {
     try {
         // const inputValue = document.getElementById('inputCode').value;
@@ -43,10 +6,13 @@ async function sendData() {
         // 입력한 텍스트를 로컬 스토리지에 저장하여 나중에 유지합니다.
         localStorage.setItem('savedCode', inputValue);
 
-        console.log(inputValue);
+        const currentPage = window.location.pathname.split('/').pop();
+        const endpoint = currentPage === 'index.html' ? '/index.html' : '/next_problem.html'
+
+        console.log(inputValue); // for debugging delete later
 
         // 서버로 입력한 정수 보내기
-        const response = await fetch('http://localhost:8080', {
+        const response = await fetch(`http://localhost:8080${endpoint}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'text/plain;charset=UTF-8',
@@ -77,11 +43,13 @@ document.addEventListener("DOMContentLoaded", async function() {
     const savedCode = await localStorage.getItem('savedCode');
     if (savedCode) {
         document.getElementById('inputCode').value = savedCode;
+        editor.setValue(savedCode);
     }
 });
 
 async function clearData() {
-    document.getElementById('inputCode').value = '';
+    // document.getElementById('inputCode').value = '';
+    editor.setValue("");
     document.getElementById('result').textContent = '';
     localStorage.removeItem('savedCode');
     localStorage.removeItem('lastResult');
